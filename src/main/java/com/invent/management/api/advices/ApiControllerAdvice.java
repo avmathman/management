b420f7f617ab2387333ecdde5caf2ec959eb9727
Abdurashid Vakhobov <avcoding01@gmail.com>
2023-06-28 19:20:37 +0500

    Implements rest controller advice.

---
+++ src/main/java/com/invent/management/api/advices/ApiControllerAdvice.java
@@ -0,0 +1,35 @@
+package com.invent.management.api.advices;
+
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+import org.springframework.web.context.request.WebRequest;
+import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
+
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
+@RestControllerAdvice
+public class ApiControllerAdvice extends ResponseEntityExceptionHandler {
+
+    /**
+     * Handles DataIntegrityViolationException thrown by Rest API methods.
+     *
+     * @param ex - exception instance.
+     * @param request - request instance.
+     */
+    @ResponseStatus(HttpStatus.CONFLICT)
+    @ResponseBody
+    @ExceptionHandler(DataIntegrityViolationException.class)
+    public ResponseEntity<Object> handleDataIntegrityViolationException(DataIntegrityViolationException ex, WebRequest request) {
+        log.error("An error occurred while execution of operation {}: {}", request.getContextPath(), ex.getMessage());
+
+        final ApiErrorResponse apiError = new ApiErrorResponse(HttpStatus.CONFLICT, ex.getLocalizedMessage());
+        return handleExceptionInternal(ex, apiError, new HttpHeaders(), apiError.getStatus(), request);
+    }
+}
---
+++ src/main/java/com/invent/management/api/advices/ApiErrorResponse.java
@@ -0,0 +1,37 @@
+package com.invent.management.api.advices;
+
+import org.springframework.http.HttpStatus;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+/**
+ * Common API error response object.
+ */
+@Getter
+@Setter
+@NoArgsConstructor
+class ApiErrorResponse {
+
+    /**
+     * Creates new instance of API error response
+     *
+     * @param status - the HTTP status code related to the error.
+     * @param message - the error message occurred.
+     */
+    ApiErrorResponse(HttpStatus status, String message) {
+        this.status = status;
+        this.message = message;
+    }
+
+    /**
+     * The HTTP status code related to the error.
+     */
+    private HttpStatus status;
+
+    /**
+     * The error message occurred.
+     */
+    private String message;
+}
