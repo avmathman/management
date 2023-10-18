9198da58f23737762793557a5a5364fcefd27eca
Abdurashid Vakhobov <102615242+avmathman@users.noreply.github.com>
2023-10-18 15:35:28 +0500

    Liquibase (#9)
    * Sets liquibase for creating tables for roles and users
    
    * Inserts one default user and two default roles

--- src/main/java/com/invent/management/data/user/UserEntity.java
+++ src/main/java/com/invent/management/data/user/UserEntity.java
@@ -2,12 +2,7 @@ package com.invent.management.data.user;
 
 import java.sql.Timestamp;
 
-import javax.persistence.Column;
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
-import javax.persistence.Table;
+import javax.persistence.*;
 import javax.validation.constraints.NotNull;
 
 import org.hibernate.annotations.CreationTimestamp;
