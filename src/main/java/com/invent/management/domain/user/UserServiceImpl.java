cab82b21e5d39f3bd0c8f666318f454fbe0a1b46
Abdurashid Vakhobov <102615242+avmathman@users.noreply.github.com>
2023-10-20 12:59:13 +0500

    Implements actuator (#10)

--- src/main/java/com/invent/management/domain/user/UserServiceImpl.java
+++ src/main/java/com/invent/management/domain/user/UserServiceImpl.java
@@ -11,7 +11,6 @@ import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
-import java.util.Objects;
 
 /**
  * {@link UserService} implementation.
